ec7dd65516e28b2d4fc4a22282472976e8f78886
Casey R <crowlandsdev@gmail.com>
2021-09-23 14:58:13 -0400

    created a fetch command to grab the data from the public folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import { React, useState, useCallback, useEffect} from "react";
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointment";
-import appointmentList from "./data.json";
 import AppointmentInfo from "./components/AppointmentInfo";
 import { BiCalendar, BiTrash } from "react-icons/bi";
 
 function App() {
+
+  const [appointmentList, setAppointmentList] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data)
+      })
+  }, []);
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData]);
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
