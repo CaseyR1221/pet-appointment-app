7ed27959279a8859cc9bf49a9f4c2b26333b5ecf
Casey R <crowlandsdev@gmail.com>
2021-09-23 17:04:15 -0400

    finished appointment form and made it submittable

--- src/components/AddAppointment.js
+++ src/components/AddAppointment.js
@@ -1,8 +1,31 @@
 import { React, useState } from "react";
 import { BiCalendarPlus } from "react-icons/bi";
 
-const AddAppointment = () => {
+const AddAppointment = ({ onSendAppointment, lastId }) => {
+
+  const clearData = {
+    ownerName: '',
+    petName: '',
+    aptDate: '',
+    aptTime: '',
+    aptNotes: ''
+  }
+
+  const formDataPublish = () => {
+    const appointmentInfo = {
+      id: lastId + 1,
+      ownerName: formData.ownerName,
+      petName: formData.petName,
+      aptDate: formData.aptDate + ' ' + formData.aptTime,
+      aptNotes: formData.aptNotes
+    }
+    onSendAppointment(appointmentInfo);
+    setFormData(clearData);
+    setToggleForm(!toggleForm)
+  }
+
   const [toggleForm, setToggleForm] = useState(false);
+  const [formData, setFormData] = useState(clearData);
 
   return (
     <div>
@@ -31,6 +54,8 @@ const AddAppointment = () => {
                 name="ownerName"
                 id="ownerName"
                 className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                onChange={(event) => { setFormData({ ...formData, ownerName: event.target.value }) }}
+                value={formData.ownerName}
               />
             </div>
           </div>
@@ -48,6 +73,8 @@ const AddAppointment = () => {
                 name="petName"
                 id="petName"
                 className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                onChange={(event) => { setFormData({ ...formData, petName: event.target.value }) }}
+                value={formData.petName}
               />
             </div>
           </div>
@@ -65,6 +92,8 @@ const AddAppointment = () => {
                 name="aptDate"
                 id="aptDate"
                 className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                onChange={(event) => { setFormData({ ...formData, aptDate: event.target.value }) }}
+                value={formData.aptDate}
               />
             </div>
           </div>
@@ -82,6 +111,8 @@ const AddAppointment = () => {
                 name="aptTime"
                 id="aptTime"
                 className="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                onChange={(event) => { setFormData({ ...formData, aptTime: event.target.value }) }}
+                value={formData.aptTime}
               />
             </div>
           </div>
@@ -100,6 +131,8 @@ const AddAppointment = () => {
                 rows="3"
                 className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md"
                 placeholder="Detailed comments about the condition"
+                onChange={(event) => { setFormData({ ...formData, aptNotes: event.target.value }) }}
+                value={formData.aptNotes}
               ></textarea>
             </div>
           </div>
@@ -109,6 +142,7 @@ const AddAppointment = () => {
               <button
                 type="submit"
                 className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-400 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400"
+                onClick={formDataPublish}
               >
                 Submit
               </button>
